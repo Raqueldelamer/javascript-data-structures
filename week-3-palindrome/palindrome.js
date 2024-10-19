de179120c94e137e95ca135ef39ec13630148d17
Raquel Marie <raqueldelamer@gmail.com>
2024-10-19 10:43:38 -0500

    update palindrome.js

--- week-3-palindrome/palindrome.js
+++ week-3-palindrome/palindrome.js
@@ -13,21 +13,22 @@ function isPalindrome(str) {
   let stack ;
   let queue ;
 
-  // TODO: Step 1: Sanitize the input
+  // TODO: Step 1: Sanitize the input (BONUS)
   let sanitizedStr; 
 
   // TODO: Step 2: Push characters onto stack and enqueue them into queue
   for (let i=0; i< sanitizedStr.length; i++) {
     let char = sanitizedStr[i]; 
 
-
+    stack.push(char)
+    queue.enqueue(char)
 
   }
 
   // TODO: Step 3: Compare characters by popping from stack and dequeuing from queue
   while (!stack.isEmpty()) {
-    let a;
-    let b;
+    let a = [1,2,3,4,5];
+    let b = [1,2,3,4,5]
 
       if (a !== b) {
           return "The string is not a palindrome.";
