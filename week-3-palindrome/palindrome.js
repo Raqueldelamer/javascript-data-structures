acc8e760445e81bcbaaedb59198e2aa29c81a7d6
Raquel Marie <raqueldelamer@gmail.com>
2024-10-20 18:43:18 -0500

    working on palindromes

--- week-3-palindrome/palindrome.js
+++ week-3-palindrome/palindrome.js
@@ -5,8 +5,36 @@ class Queue {
 }
 
 class Stack {
-
+  constructor() {
+      this.items = [];
+  }
+  push(names) {
+      this.items.push(names);
+  }
+  pop() {
+      return this.items.pop();
+  }
+  peek() {
+      return this.items[0];
+  }
+  isEmpty() {
+      return this.items.length === 0;
+  };
 }
+let s = new Stack();
+s.push("Alice");
+s.push("Beatrice");
+s.push("Caroline");
+console.log(s);
+s.push("David");
+console.log(s);
+
+let first = s.pop();
+console.log(first);
+
+let last = s.peek();
+console.log(last);
+console.log(s.isEmpty());
 
 function isPalindrome(str) {
   // TODO: instantiate the stack and queue
